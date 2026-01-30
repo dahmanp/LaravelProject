b9cf9bf06ac28e2d880b0afc6a5e5b5ebe5b8179
dahmanp <dahmanp@my.erau.edu>
2026-01-30 15:48:29 -0700

    Continue Laravel

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,33 +2,20 @@
 
 namespace App\Models;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Job extends Model {
+    use HasFactory;
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
     }
 
-    public static function find(int $id): array {
-        $job = \Illuminate\Support\Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
+    public function tags() {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
     }
 }
